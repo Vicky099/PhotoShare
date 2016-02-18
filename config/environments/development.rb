115e634ce129f135e94428d145e85b874a28f650
Vikram <vikky.panmand@gmail.com>
2016-02-18 13:08:15 +0530

    remove id

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :port           => 587,
     :address        => 'smtp.gmail.com',
-    :user_name      => 'amardada001@gmail.com',
-    :password       => 'vikramraje',
+    :user_name      => '',
+    :password       => '',
     :domain         => 'gmail.com',
     :authentication => :plain,
     :enable_starttls_auto => true
